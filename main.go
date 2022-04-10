6c29d1d688350f9fb5ca1123f339cccab6c4902b
shun0305 <shunbbra@icloud.com>
2022-04-10 15:44:08 +0900

    struct block

--- main.go
+++ main.go
@@ -2,10 +2,32 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"time"
 )
 
+type Block struct {
+	nonce        int
+	previousHash string
+	timestamp    int64
+	transactions []string
+}
+
+func NewBlock(nonce int, previousHash string) *Block {
+	b := new(Block)
+	b.timestamp = time.Now().UnixNano()
+	b.nonce = nonce
+	b.previousHash = previousHash
+	return b
+}
+
+func (b *Block) Print() {
+	fmt.Printf("timestamp    %d\n", b.timestamp)
+	fmt.Printf("nonce        %d\n", b.nonce)
+	fmt.Printf("previousHash %s\n", b.previousHash)
+	fmt.Printf("transactions %s\n", b.transactions)
+}
+
 func main() {
-	log.Println("test")
-	fmt.Println("test")
+	b := NewBlock(0, "init hash")
+	b.Print()
 }
